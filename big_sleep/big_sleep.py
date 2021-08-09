4ebe14f33e05dfb72022d2322dad73f367ba02e1
Jonah Mackey <79280939+jonahmackey@users.noreply.github.com>
2021-08-09 11:53:42 -0400

    Update big_sleep.py

--- big_sleep/big_sleep.py
+++ big_sleep/big_sleep.py
@@ -871,14 +871,14 @@ class Imagine(nn.Module):
                 if self.multiple:
                     self.model.model.latents3.train()
 
-                save_image(bg_image, str(self.bg_filename))
-                save_image(fg_image, str(self.fg_filename))
-                save_image(comp_image, str(self.comp_filename))
-                if self.multiple:
-                    save_image(bg2_image, str(self.bg2_filename))
-                    save_image(comp2_image, str(self.comp2_filename))
-                if self.fixed_alpha is None:
-                    save_image(alpha_image, str(self.alpha_filename))
+#                 save_image(bg_image, str(self.bg_filename))
+#                 save_image(fg_image, str(self.fg_filename))
+#                 save_image(comp_image, str(self.comp_filename))
+#                 if self.multiple:
+#                     save_image(bg2_image, str(self.bg2_filename))
+#                     save_image(comp2_image, str(self.comp2_filename))
+#                 if self.fixed_alpha is None:
+#                     save_image(alpha_image, str(self.alpha_filename))
                     
                 if self.save_grid: 
                     # saves a grid of images in the form: bg, bg2, comp, comp2, fg, alpha
@@ -900,26 +900,26 @@ class Imagine(nn.Module):
                     num = total_iterations // self.save_every
     
                     if self.save_dir is not None:
-                        save_image(bg_image, Path(f'./{self.save_dir}/{self.bg_text_path}.{num}{self.seed_suffix}.png'))
-                        save_image(fg_image, Path(f'./{self.save_dir}/' + 'fg' + f'.{num}{self.seed_suffix}.png'))
-                        save_image(comp_image, Path(f'./{self.save_dir}/{self.comp_text_path}.{num}{self.seed_suffix}.png'))
-                        if self.multiple:
-                            save_image(bg2_image, Path(f'./{self.save_dir}/{self.bg2_text_path}.{num}{self.seed_suffix}.png'))
-                            save_image(comp2_image, Path(f'./{self.save_dir}/{self.comp2_text_path}.{num}{self.seed_suffix}.png'))
-                        if self.fixed_alpha is None:
-                            save_image(alpha_image, Path(f'./{self.save_dir}/' + 'alpha' + f'.{num}{self.seed_suffix}.png'))
+#                         save_image(bg_image, Path(f'./{self.save_dir}/{self.bg_text_path}.{num}{self.seed_suffix}.png'))
+#                         save_image(fg_image, Path(f'./{self.save_dir}/' + 'fg' + f'.{num}{self.seed_suffix}.png'))
+#                         save_image(comp_image, Path(f'./{self.save_dir}/{self.comp_text_path}.{num}{self.seed_suffix}.png'))
+#                         if self.multiple:
+#                             save_image(bg2_image, Path(f'./{self.save_dir}/{self.bg2_text_path}.{num}{self.seed_suffix}.png'))
+#                             save_image(comp2_image, Path(f'./{self.save_dir}/{self.comp2_text_path}.{num}{self.seed_suffix}.png'))
+#                         if self.fixed_alpha is None:
+#                             save_image(alpha_image, Path(f'./{self.save_dir}/' + 'alpha' + f'.{num}{self.seed_suffix}.png'))
                         if self.save_grid: 
                             # saves a grid of images in the form: bg, bg2, comp, comp2, fg, alpha
                             save_image(grid_image, Path(f'./{self.save_dir}/' + 'grid' + f'.{num}{self.seed_suffix}.png'))
                     else:
-                        save_image(bg_image, Path(f'./{self.bg_text_path}.{num}{self.seed_suffix}.png'))
-                        save_image(fg_image, Path('./fg' + f'.{num}{self.seed_suffix}.png'))
-                        save_image(comp_image, Path(f'./{self.comp_text_path}.{num}{self.seed_suffix}.png'))
-                        if self.multiple:
-                            save_image(bg2_image, Path(f'./{self.bg2_text_path}.{num}{self.seed_suffix}.png'))
-                            save_image(comp2_image, Path(f'./{self.comp2_text_path}.{num}{self.seed_suffix}.png'))
-                        if self.fixed_alpha is None:
-                            save_image(alpha_image, Path('./alpha' + f'.{num}{self.seed_suffix}.png'))
+#                         save_image(bg_image, Path(f'./{self.bg_text_path}.{num}{self.seed_suffix}.png'))
+#                         save_image(fg_image, Path('./fg' + f'.{num}{self.seed_suffix}.png'))
+#                         save_image(comp_image, Path(f'./{self.comp_text_path}.{num}{self.seed_suffix}.png'))
+#                         if self.multiple:
+#                             save_image(bg2_image, Path(f'./{self.bg2_text_path}.{num}{self.seed_suffix}.png'))
+#                             save_image(comp2_image, Path(f'./{self.comp2_text_path}.{num}{self.seed_suffix}.png'))
+#                         if self.fixed_alpha is None:
+#                             save_image(alpha_image, Path('./alpha' + f'.{num}{self.seed_suffix}.png'))
                         if self.save_grid: 
                             # saves a grid of images in the form: bg, bg2, comp, comp2, fg, alpha
                             save_image(grid_image, Path('./grid' + f'.{num}{self.seed_suffix}.png'))
@@ -927,28 +927,28 @@ class Imagine(nn.Module):
                 if self.save_best and top_score.item() < self.current_best_score:
                     self.current_best_score = top_score.item()
     
-                    if self.save_dir is not None:
-                        save_image(bg_image, Path(f'./{self.save_dir}/{self.bg_text_path}{self.seed_suffix}.png'))
-                        save_image(fg_image, Path(f'./{self.save_dir}/' + 'fg' + f'{self.seed_suffix}.png'))
-                        save_image(comp_image, Path(f'./{self.save_dir}/{self.comp_text_path}{self.seed_suffix}.png'))
-                        if self.multiple:
-                            save_image(bg2_image, Path(f'./{self.save_dir}/{self.bg2_text_path}{self.seed_suffix}.png'))
-                            save_image(comp2_image, Path(f'./{self.save_dir}/{self.comp2_text_path}{self.seed_suffix}.png'))
-                        if self.fixed_alpha is None:
-                            save_image(alpha_image, Path(f'./{self.save_dir}/' + 'alpha' + f'{self.seed_suffix}.png'))
+#                     if self.save_dir is not None:
+#                         save_image(bg_image, Path(f'./{self.save_dir}/{self.bg_text_path}{self.seed_suffix}.png'))
+#                         save_image(fg_image, Path(f'./{self.save_dir}/' + 'fg' + f'{self.seed_suffix}.png'))
+#                         save_image(comp_image, Path(f'./{self.save_dir}/{self.comp_text_path}{self.seed_suffix}.png'))
+#                         if self.multiple:
+#                             save_image(bg2_image, Path(f'./{self.save_dir}/{self.bg2_text_path}{self.seed_suffix}.png'))
+#                             save_image(comp2_image, Path(f'./{self.save_dir}/{self.comp2_text_path}{self.seed_suffix}.png'))
+#                         if self.fixed_alpha is None:
+#                             save_image(alpha_image, Path(f'./{self.save_dir}/' + 'alpha' + f'{self.seed_suffix}.png'))
                         if self.save_grid: 
                             # saves a grid of images in the form: bg, bg2, comp, comp2, fg, alpha
                             save_image(grid_image, Path(f'./{self.save_dir}/' + 'grid' + f'{self.seed_suffix}.png'))
                         
                     else:
-                        save_image(bg_image, Path(f'./{self.bg_text_path}{self.seed_suffix}.png'))
-                        save_image(fg_image, Path('./fg' + f'{self.seed_suffix}.png'))
-                        save_image(comp_image, Path(f'./{self.comp_text_path}{self.seed_suffix}.png'))
-                        if self.multiple:
-                            save_image(bg2_image, Path(f'./{self.bg2_text_path}{self.seed_suffix}.png'))
-                            save_image(comp2_image, Path(f'./{self.comp2_text_path}{self.seed_suffix}.png'))
-                        if self.fixed_alpha is None:
-                            save_image(alpha_image, Path('./alpha' + f'{self.seed_suffix}.png'))
+#                         save_image(bg_image, Path(f'./{self.bg_text_path}{self.seed_suffix}.png'))
+#                         save_image(fg_image, Path('./fg' + f'{self.seed_suffix}.png'))
+#                         save_image(comp_image, Path(f'./{self.comp_text_path}{self.seed_suffix}.png'))
+#                         if self.multiple:
+#                             save_image(bg2_image, Path(f'./{self.bg2_text_path}{self.seed_suffix}.png'))
+#                             save_image(comp2_image, Path(f'./{self.comp2_text_path}{self.seed_suffix}.png'))
+#                         if self.fixed_alpha is None:
+#                             save_image(alpha_image, Path('./alpha' + f'{self.seed_suffix}.png'))
                         if self.save_grid: 
                             # saves a grid of images in the form: bg, bg2, comp, comp2, fg, alpha
                             save_image(grid_image, Path('./grid' + f'{self.seed_suffix}.png'))
